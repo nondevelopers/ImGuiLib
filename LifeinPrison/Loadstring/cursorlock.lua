06b7289f576095dcf0c168b57091a57fb097d181
@xqzxa <mozaireal@gmail.com>
2026-06-20 14:37:52 -0400

    Delete LifeinPrison/Loadstring directory

--- LifeinPrison/Loadstring/cursorlock.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xqzxa/ImGuiLib/main/scripts/cursorlock.lua"))()
--- LifeinPrison/Loadstring/teamswticher.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xqzxa/ImGuiLib/main/LifeinPrison/TeamSwitcher.lua"))()
